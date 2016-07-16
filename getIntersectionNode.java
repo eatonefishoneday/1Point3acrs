38dd722c0dcd1f5cdf5d67387c22f25ab61f2701
eatonefishoneday <334686971@qq.com>
2016-07-16 11:54:26 +0800

    Update getIntersectionNode.java

--- getIntersectionNode.java
+++ getIntersectionNode.java
@@ -9,12 +9,16 @@
  *     }
  * }
  */
+ //headA headB和后面的C连起来可以看作两个linked list 长度可能相同/不同
+ //如果相同就好做了，相同的话直接判断A.next == B.next 相等就返回
+ //如果不同，可以算出A和B的长度的差值，然后让长的list那个从差值的位置next.短的从起点next
+ //当两个next相等时，就是交点
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB){
         if (headA == null || headB == null) {
             return null;
         }
-    ListNode nodeA = headA,    
+    ListNode nodeA = headA,     
              nodeB= headB;
     int lengthA = 1, 
         lengthB = 1;
